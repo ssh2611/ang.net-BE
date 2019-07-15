7d37a00e4d96dca62b93fb677026a8454c83fb4e
shreyas <shreyas>
2019-07-15 18:37:15 +0200

    Initial Backend

---
+++ Controllers/MessagesController.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MessageBoardBackend.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Messages")]
+    public class MessagesController : Controller
+    {
+        [HttpGet]
+        public IEnumerable<Models.Message> Get()
+        {
+            return new Models.Message[] {
+                new Models.Message {
+                    Owner = "John",
+                    Text = "hello"
+                },
+                new Models.Message
+                {
+                    Owner = "Tim",
+                    Text = "Hi"
+                }
+            };
+        }
+        [HttpPost]
+        public void post([FromBody] Models.Message message)
+        {
+            
+        }
+    }
+}
